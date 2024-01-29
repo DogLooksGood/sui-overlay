82e51b7b7d46de0a7e32e16911e8e506a7dbf9b5
Linerre <leon.errelin@outlook.com>
2024-01-29 16:00:34 +1100

    Bump version: Sui and nixpkgs
    * Sui devnet and testnet to v17
    * Sui mainnet to v16
    * Include other binaries such as faucet and move-analyzer
    * Fix README Usage

--- overlays/sui-devnet.nix
+++ overlays/sui-devnet.nix
@@ -1,13 +1,23 @@
 pkgs: pkgs.stdenv.mkDerivation {
   name = "sui-devnet";
 
+  # Pass stripRoot=false; to fetchzip to assume flat list of files.
+  # devnet
+  # |--target/release/*
+  # |--external-crates/move/target/release/move-analyzer
   src = pkgs.fetchzip {
-    url = "https://github.com/MystenLabs/sui/releases/download/devnet-v1.7.0/sui-devnet-v1.7.0-ubuntu-x86_64.tgz";
-    sha256 = "sha256-/HwyetDYcxjdKgopdaC+ionsuW/Qd9I3VJOcNgz8TPs=";
+    stripRoot = false;
+    url = "https://github.com/MystenLabs/sui/releases/download/devnet-v1.17.0/sui-devnet-v1.17.0-ubuntu-x86_64.tgz";
+    sha256 = "sha256-pAduyLgqJINzW0nDky1zbo6ZaXcm7hAxOQ6+E2/q4LY=";
+
   };
 
   installPhase = ''
     mkdir -p $out/bin
-    cp $src/release/sui-ubuntu-x86_64 $out/bin/sui
+    cp $src/external-crates/move/target/release/move-analyzer-ubuntu-x86_64 $out/bin/move-analyzer
+    cd $src/target/release
+    for b in *; do
+        cp ''${b} $out/bin/''${b%-ubuntu-x86_64}
+    done
   '';
 }
--- overlays/sui-testnet.nix
+++ overlays/sui-testnet.nix
@@ -2,12 +2,17 @@ pkgs: pkgs.stdenv.mkDerivation {
   name = "sui-testnet";
 
   src = pkgs.fetchzip {
-    url = "https://github.com/MystenLabs/sui/releases/download/testnet-v1.7.0/sui-testnet-v1.7.0-ubuntu-x86_64.tgz";
-    sha256 = "sha256-agJl/rml/oLeEBn7BBmnhGKZFMxpTP3qedmaqrpATmk=";
+    stripRoot = false;
+    url = "https://github.com/MystenLabs/sui/releases/download/testnet-v1.17.0/sui-testnet-v1.17.0-ubuntu-x86_64.tgz";
+    sha256 = "sha256-qCOpM//qRbjZBkJpoNEfyp44YPlQ3wlE5qgRNIGwk6E=";
   };
 
   installPhase = ''
     mkdir -p $out/bin
-    cp $src/release/sui-ubuntu-x86_64 $out/bin/sui
+    cp $src/external-crates/move/target/release/move-analyzer-ubuntu-x86_64 $out/bin/move-analyzer
+    cd $src/target/release
+    for b in *; do
+        cp ''${b} $out/bin/''${b%-ubuntu-x86_64}
+    done
   '';
 }
